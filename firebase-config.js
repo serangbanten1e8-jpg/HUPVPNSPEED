3e32ffa596361e159643be40a7aa151d9fb1c61f
serangbanten1e8-jpg <serangbanten1e8@gmail.com>
2025-08-22 16:41:38 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
