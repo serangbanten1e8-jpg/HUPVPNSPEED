9351edc908370e6a35bdf00ef85f7127c7ec1c0e
serangbanten1e8-jpg <serangbanten1e8@gmail.com>
2025-08-22 18:03:34 +0700

    auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const admin = require('../firebase-config');
 
-module.exports = async function(req, res, next) {
+module.exports = async (req, res, next) => {
   const token = req.headers.authorization?.split('Bearer ')[1];
   if (!token) return res.status(401).json({ error: 'Token dibutuhkan' });
 
@@ -8,7 +8,7 @@ module.exports = async function(req, res, next) {
     const decoded = await admin.auth().verifyIdToken(token);
     req.user = decoded;
     next();
-  } catch (err) {
+  } catch {
     res.status(401).json({ error: 'Token tidak valid' });
   }
 };
