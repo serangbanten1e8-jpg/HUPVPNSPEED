ec59e97b59a6b0b8ff25f338b5377ab407caca91
serangbanten1e8-jpg <serangbanten1e8@gmail.com>
2025-08-22 18:02:54 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON || '{}');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://<your-project-id>.firebaseio.com'
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
 
 module.exports = admin;
